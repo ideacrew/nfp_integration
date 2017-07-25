50082c703bcf519e9f422c46cc3dbdeabbc63a9f
Antonio Schaffert <antonio.schaffert@gmail.com>
2017-07-25 09:52:08 -0400

    .

--- lib/nfp_integration/railties/railtie.rb
+++ lib/nfp_integration/railties/railtie.rb
@@ -1,12 +1,14 @@
-
 module NfpIntegration
   module Railties
     class Railtie < Rails::Railtie
 
-      initializer "nfp_soap_security" do |app|
+      initializer "railtie.nfp_soap_security" do |app|
+
         puts "Init Variables for NFP SOAP Services"
+
       end
-      
+
+
     end
   end
 end
